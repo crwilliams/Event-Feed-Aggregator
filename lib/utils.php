697c748942540e5ffb6a4cc5b15c18481d7bcbc5
Colin Williams <github@crwilliams.co.uk>
2012-02-25 19:18:07 +0000

    Added missing licensing lines.

--- lib/utils.php
+++ lib/utils.php
@@ -12,6 +12,10 @@
 # Event Feed Aggregator is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Event Feed Aggregator.  If not, see <http://www.gnu.org/licenses/>.
 
 date_default_timezone_set('Europe/London');
 require_once $diary_config["path"].'/lib/xml.php';
