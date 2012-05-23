bb9288030dc6736e6a1f977e632c339e0203d2f4
Colin Williams <github@crwilliams.co.uk>
2012-05-23 13:27:09 +0100

    fixed lib path

--- diary.soton.ac.uk/htdocs/feeds.html.php
+++ diary.soton.ac.uk/htdocs/feeds.html.php
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Event Feed Aggregator.  If not, see <http://www.gnu.org/licenses/>.
 
-require('../../../etc/config.php');
+require('../../etc/config.php');
 
 $fields = array(
 'FeedID',
