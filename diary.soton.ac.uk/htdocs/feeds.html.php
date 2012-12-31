320ccad22b17f88f96466b08e23c849253df13a2
Colin Williams <github@crwilliams.co.uk>
2012-12-31 16:04:51 +0000

    Added counts of events, event instances and future event instances.

--- diary.soton.ac.uk/htdocs/feeds.html.php
+++ diary.soton.ac.uk/htdocs/feeds.html.php
@@ -18,6 +18,7 @@
 # along with Event Feed Aggregator.  If not, see <http://www.gnu.org/licenses/>.
 
 require('../../etc/config.php');
+require('dev/inc/functions.php');
 
 $fields = array(
 'FeedID',
@@ -79,6 +80,7 @@ tr.script_ td {
 <h1>Current Sources of Data for the Events Calendar</h1>
 <table class='datagrid'>
 <?php
+list($eventcount, $eventinstancecount, $eventfutureinstancecount) = getCounts();
 $i = 0;
 foreach( $feeds as $feed )
 {
@@ -87,6 +89,9 @@ foreach( $feeds as $feed )
 	{
 		print "<tr>";
 		foreach( $fields as $field_name ) { print "<th>$field_name</th>"; }
+		print "<th>Events</th>";
+		print "<th>Event Instances</th>";
+		print "<th>Future Event Instances</th>";
 		print "</tr>";
 	}
 	print "<tr class='script_".$feed["Script"]."'>";
@@ -100,8 +105,51 @@ foreach( $feeds as $feed )
 			print "<td>".$feed[$field_name]."</td>"; 
 		}
 	}
+	print "<td>".$eventcount['http://id.southampton.ac.uk/diary/'.preg_replace('/[^A-Za-z0-9]/', '', $feed['FeedID'])]."</td>";
+	print "<td>".$eventinstancecount['http://id.southampton.ac.uk/diary/'.preg_replace('/[^A-Za-z0-9]/', '', $feed['FeedID'])]."</td>";
+	print "<td>".$eventfutureinstancecount['http://id.southampton.ac.uk/diary/'.preg_replace('/[^A-Za-z0-9]/', '', $feed['FeedID'])]."</td>";
 	print "</tr>";
 	++$i;
 }
 print "</table>";
 print "</html>";
+
+function getCounts()
+{
+	$graph = getGraph();
+        foreach($graph->allOfType("event:Event") as $event)
+        {
+		foreach($event->all("-http://purl.org/prog/has_event") as $feed)
+		{
+			$eventcount[(string)$feed]++;
+		}
+                if($event->has("event:time"))
+                {
+                        foreach($event->all("event:time") as $time)
+                        {
+				foreach($event->all("-http://purl.org/prog/has_event") as $feed)
+				{
+					$eventinstancecount[(string)$feed]++;
+                                	if($time->has("tl:at"))
+                                	{
+						//echo $time->getString("tl:at");
+						if($time->getString("tl:at") > date('Y-m-d'))
+						{
+							$eventfutureinstancecount[(string)$feed]++;
+						}
+                                	}
+                                	elseif($time->has("tl:start"))
+                                	{
+						//echo $time->getString("tl:start");
+						if(substr($time->getString("tl:start"), 0, 10) > date('Y-m-d'))
+						{
+							$eventfutureinstancecount[(string)$feed]++;
+						}
+                                	}
+				}
+                        }
+                }
+        }
+	return array($eventcount, $eventinstancecount, $eventfutureinstancecount);
+}
+
